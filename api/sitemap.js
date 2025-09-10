48b423923c05a530a1d73f2207b1b5d0d9dd86a6
yarrowmartin3-prog <yarrowmartin3@gmail.com>
2025-09-10 04:31:05 -0400

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,17 +1,13 @@
-
 export const config = { runtime: "edge" };
-
-// Génère un mini sitemap qui référence l'accueil et les 2 endpoints API
-export default async function handler(req) {
+export default async function handler(req){
   const host = req.headers.get("x-forwarded-host") || req.headers.get("host") || "";
   const proto = req.headers.get("x-forwarded-proto") || "https";
   const origin = `${proto}://${host}`;
-
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url><loc>${origin}/</loc></url>
   <url><loc>${origin}/api/posts</loc></url>
   <url><loc>${origin}/api/curate</loc></url>
 </urlset>`;
-  return new Response(xml, { status: 200, headers: { "Content-Type": "application/xml" } });
+  return new Response(xml,{status:200,headers:{ "Content-Type":"application/xml" }});
 }
